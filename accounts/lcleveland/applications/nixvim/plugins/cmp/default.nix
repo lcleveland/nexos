913e86d9a8f6319af818e76ba0235d8c95e6b898
lcleveland <lyle.cleveland@proton.me>
2024-11-23 17:21:54 -0600

    add imports

--- accounts/lcleveland/applications/nixvim/plugins/cmp/default.nix
+++ accounts/lcleveland/applications/nixvim/plugins/cmp/default.nix
@@ -2,5 +2,7 @@
 { ... }: {
   imports = [
     (import ./enable.nix { username = username; plugin_name = plugin_name; })
+    (import ./mappings.nix { username = username; plugin_name = plugin_name; })
+    (import ./sources.nix { username = username; plugin_name = plugin_name; })
   ];
 }
--- accounts/lcleveland/applications/nixvim/plugins/cmp/sources.nix
+++ accounts/lcleveland/applications/nixvim/plugins/cmp/sources.nix
@@ -6,13 +6,15 @@ let
 in
 {
   options.accounts.${username}.applications.nixvim.plugins.${plugin_name} = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = lib.mdDoc "Enable ${plugin_name} plugin";
+    sources = lib.mkOption {
+      type = lib.types.listOf lib.types.attrs;
+      default = [
+        { }
+      ];
+      description = lib.mdDoc "Sources for cmp plugin";
     };
   };
   config = lib.mkIf (account.enable && nixvim.enable) {
-    home-manager.users.${username}.programs.nixvim.plugins.${plugin_name}.enable = nixvim.plugins.${plugin_name}.enable;
+    home-manager.users.${username}.programs.nixvim.plugins.${plugin_name}.settings.sources = nixvim.plugins.${plugin_name}.sources;
   };
 }
