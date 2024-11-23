34b34405b39906fdd9e1c80f77939497ac21a8fd
lcleveland <lyle.cleveland@proton.me>
2024-11-23 14:06:14 -0600

    try zen kernel

--- profiles/dream.nix
+++ profiles/dream.nix
@@ -1,7 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./hardware_configurations/dream.nix
   ];
   accounts.lcleveland.settings.enable = true;
   system.settings.graphics.enable = true;
+  system.settings.boot.kernel.package = pkgs.linuxPackages_zen;
 }
