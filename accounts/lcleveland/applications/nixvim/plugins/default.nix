8286fee76eb1104aecafff7924ef8e65ce7e6d87
lcleveland <lyle.cleveland@proton.me>
2024-11-23 17:29:43 -0600

    add cmp nvim lsp

--- accounts/lcleveland/applications/nixvim/plugins/default.nix
+++ accounts/lcleveland/applications/nixvim/plugins/default.nix
@@ -1,6 +1,7 @@
 { ... }: {
   imports = [
     (import ./cmp { username = "lcleveland"; plugin_name = "cmp"; })
+    (import ./cmp-nvim-lsp { username = "lcleveland"; plugin_name = "cmp-nvim-lsp"; })
     (import ./codeium-nvim { username = "lcleveland"; plugin_name = "codeium-nvim"; })
   ];
 }
