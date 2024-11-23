66c789aa66e4563fce1e7baa09a45f3f0311e96a
lcleveland <lyle.cleveland@proton.me>
2024-11-23 13:05:32 -0600

    change logic

--- system/settings/desktop_environment/kde/default.nix
+++ system/settings/desktop_environment/kde/default.nix
@@ -1,6 +1,14 @@
-{ ... }: {
+{ config, lib, ... }:
+let
+  desktop_environment = config.system.settings.desktop_environment;
+in
+{
   imports = [
     ./display_manager
     ./enable.nix
   ];
+  config = lib.mkIf (desktop_environment.enabled_environment == "kde") {
+    services.xserver.enable = true;
+    services.desktopManager.plasma6.enable = true;
+  };
 }
