2780065ee89bbe35cd4cc80107523089977cd054
lcleveland <lyle.cleveland@proton.me>
2024-11-23 17:51:42 -0600

    add lsp-format

--- accounts/lcleveland/applications/nixvim/plugins/default.nix
+++ accounts/lcleveland/applications/nixvim/plugins/default.nix
@@ -4,5 +4,6 @@
     (import ./cmp-nvim-lsp { username = "lcleveland"; plugin_name = "cmp-nvim-lsp"; })
     (import ./codeium-nvim { username = "lcleveland"; plugin_name = "codeium-nvim"; })
     (import ./lsp { username = "lcleveland"; plugin_name = "lsp"; })
+    (import ./lsp-format { username = "lcleveland"; plugin_name = "lsp-format"; })
   ];
 }
