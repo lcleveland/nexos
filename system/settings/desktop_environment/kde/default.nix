d8151f3e20133178825324a26b04216fd1cef416
lcleveland <lyle.cleveland@proton.me>
2024-11-23 21:46:49 -0600

    add excluded packages

--- system/settings/desktop_environment/kde/default.nix
+++ system/settings/desktop_environment/kde/default.nix
@@ -5,6 +5,7 @@ in
 {
   imports = [
     ./display_manager
+    ./excluded_packages.nix
   ];
   config = lib.mkIf (desktop_environment.enable && desktop_environment.enabled_environment == "kde") {
     services.xserver.enable = true;
---
+++ system/settings/desktop_environment/kde/excluded_packages.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+let
+  desktop_environment = config.system.settings.desktop_environment;
+in
+{
+  options.system.settings.desktop_environment.kde = {
+    excluded_packages = lib.mkOption {
+      type = lib.types.listOf lib.types.pkgs;
+      default = [ ];
+      description = "Packages to exclude from the KDE environment";
+    };
+  };
+  config = lib.mkIf (desktop_environment.enable && desktop_environment.enabled_environment == "kde") {
+    environment.plasma6.excludePackages = with pkgs.kdePackages; [
+      kate
+    ];
+  };
+}
