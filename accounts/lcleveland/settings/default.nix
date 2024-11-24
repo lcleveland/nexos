e2a99035dcf963c0c30d2dc85c649f7a8865e82c
lcleveland <lyle.cleveland@proton.me>
2024-11-24 08:46:31 -0600

    disable for test

--- accounts/lcleveland/settings/default.nix
+++ accounts/lcleveland/settings/default.nix
@@ -6,7 +6,7 @@
     ./initial_password.nix
     ./is_normal_user.nix
     ./name.nix
-    ./plasma_manager
+    #./plasma_manager
     ./state_version.nix
     ./use_global_packages.nix
     ./username.nix
