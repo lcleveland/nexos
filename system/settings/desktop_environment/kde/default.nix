6e6da4a911447a77672e2de5c071e2551b67713b
lcleveland <lyle.cleveland@proton.me>
2024-11-24 08:52:10 -0600

    test

--- system/settings/desktop_environment/kde/default.nix
+++ system/settings/desktop_environment/kde/default.nix
@@ -6,7 +6,7 @@ in
   imports = [
     ./display_manager
     ./excluded_packages.nix
-    ./qt.nix
+    #./qt.nix
   ];
   config = lib.mkIf (desktop_environment.enable && desktop_environment.enabled_environment == "kde") {
     services.xserver.enable = true;
