39ccab9297c6c1127aa23f385b5de92a502d3c81
lcleveland <lyle.cleveland@proton.me>
2024-11-24 18:42:12 -0600

    fix path

--- accounts/lcleveland/settings/gtk.nix
+++ accounts/lcleveland/settings/gtk.nix
@@ -4,7 +4,7 @@ let
 in
 {
   config = lib.mkIf account.enable {
-    home-manager.users.lcleveland.home.gtk = {
+    home-manager.users.lcleveland.gtk = {
       enable = true;
       theme = {
         name = "Adwaita-dark";
