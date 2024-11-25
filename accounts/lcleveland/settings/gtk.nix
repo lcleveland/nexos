741fa5a67d025965e7c4d27e2d010d0896758b28
lcleveland <lyle.cleveland@proton.me>
2024-11-24 20:08:07 -0600

    use proper string

--- accounts/lcleveland/settings/gtk.nix
+++ accounts/lcleveland/settings/gtk.nix
@@ -5,7 +5,7 @@ in
 {
   config = lib.mkIf account.enable {
     home-manager = {
-      backupFileExtension = true;
+      backupFileExtension = "hm-backup";
       users.lcleveland.gtk = {
         enable = true;
         theme = {
