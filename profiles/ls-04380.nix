bc37cc4d9b55e2787b91c17f1824d426cd99e7d5
lcleveland <lyle.cleveland@proton.me>
2024-11-25 09:53:13 -0600

    fix import

--- profiles/ls-04380.nix
+++ profiles/ls-04380.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   imports = [
-    ./hardware_configurations/dream.nix
+    ./hardware_configurations/ls-04380.nix
   ];
   accounts.lcleveland.settings.enable = true;
   system.settings.boot.kernel.package = pkgs.linuxPackages_zen;
