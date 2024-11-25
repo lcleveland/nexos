cfb96b5861bdb42dbdbfb1ee00192b0c4b103b47
lcleveland <lyle.cleveland@proton.me>
2024-11-24 20:06:18 -0600

    fix file conflict

--- accounts/lcleveland/settings/gtk.nix
+++ accounts/lcleveland/settings/gtk.nix
@@ -4,11 +4,14 @@ let
 in
 {
   config = lib.mkIf account.enable {
-    home-manager.users.lcleveland.gtk = {
-      enable = true;
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome-themes-extra;
+    home-manager = {
+      backupFileExtension = true;
+      users.lcleveland.gtk = {
+        enable = true;
+        theme = {
+          name = "Adwaita-dark";
+          package = pkgs.gnome-themes-extra;
+        };
       };
     };
   };
