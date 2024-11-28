9134e43c05b2e9ac36cf0b9f15f5f52fb40808de
lcleveland <lyle.cleveland@proton.me>
2024-11-28 11:54:05 -0600

    basic gnome

---
+++ system/settings/desktop_environment/gnome/default.nix
@@ -0,0 +1,16 @@
+{ config, lib, ... }:
+let
+  desktop_environment = config.system.settings.desktop_environment;
+in
+{
+  imports = [
+    ./excluded_packages.nix
+  ];
+  config = lib.mkIf (desktop_environment.enable && desktop_environment.enabled_environment == "gnome") {
+    services.xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
+  };
+}
---
+++ system/settings/desktop_environment/gnome/excluded_packages.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+let
+  desktop_environment = config.system.settings.desktop_environment;
+in
+{
+  options.system.settings.desktop_environment.gnome = {
+    excluded_packages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = with pkgs; [
+        geary
+      ];
+      description = "Packages to exclude from the KDE environment";
+    };
+  };
+  config = lib.mkIf (desktop_environment.enable && desktop_environment.enabled_environment == "kde") {
+    environment.gnome.excludePackages = desktop_environment.kde.excluded_packages;
+    services.xserver.excludePackages = with pkgs; [ xterm ];
+  };
+}
