f65966a68a0ae642205b7c0a7c3c9725fb21d6e5
lcleveland <lyle.cleveland@proton.me>
2024-11-28 12:14:35 -0600

    add triple buffering for gnome

--- system/settings/desktop_environment/gnome/default.nix
+++ system/settings/desktop_environment/gnome/default.nix
@@ -5,6 +5,7 @@ in
 {
   imports = [
     ./excluded_packages.nix
+    ./triple_buffering.nix
   ];
   config = lib.mkIf (desktop_environment.enable && desktop_environment.enabled_environment == "gnome") {
     services.xserver = {
---
+++ system/settings/desktop_environment/gnome/triple_buffering.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+let
+  desktop_environment = config.system.settings.desktop_environment.enabled_environment;
+in
+{
+  options.system.settings.desktop_environment.gnome = {
+    triple_buffering = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Use triple buffering";
+    };
+  };
+  config = lib.mkIf (desktop_environment.enable && desktop_environment == "gnome") {
+    nixpkgs.overlays = [
+      (final: prev: {
+        mutter = prev.mutter.overrideAttrs (old: {
+          src = pkgs.fetchFromGitLab {
+            domain = "gitlab.gnome.org";
+            owner = "vanvugt";
+            repo = "mutter";
+            rev = "triple-buffering-v4-46";
+            hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
+          };
+        });
+      })
+    ];
+  };
+}
