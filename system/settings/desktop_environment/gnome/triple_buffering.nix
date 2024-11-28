32c0e7e71e25bc64ce7ac0169e5212880cee8818
lcleveland <lyle.cleveland@proton.me>
2024-11-28 13:58:18 -0600

    test

--- system/settings/desktop_environment/gnome/triple_buffering.nix
+++ system/settings/desktop_environment/gnome/triple_buffering.nix
@@ -12,15 +12,18 @@ in
   };
   config = lib.mkIf (desktop_environment.enable && desktop_environment.enabled_environment == "gnome") {
     nixpkgs.overlays = [
+      # GNOME 46: triple-buffering-v4-46
       (final: prev: {
-        mutter = prev.mutter.overrideAttrs (old: {
-          src = pkgs.fetchFromGitLab {
-            domain = "gitlab.gnome.org";
-            owner = "vanvugt";
-            repo = "mutter";
-            rev = "triple-buffering-v4-46";
-            hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
-          };
+        gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+          mutter = gnomePrev.mutter.overrideAttrs (old: {
+            src = pkgs.fetchFromGitLab {
+              domain = "gitlab.gnome.org";
+              owner = "vanvugt";
+              repo = "mutter";
+              rev = "triple-buffering-v4-46";
+              hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
+            };
+          });
         });
       })
     ];
