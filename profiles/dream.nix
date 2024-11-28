9be0259ccb012b2ec0ab7a3199bbcc6e6c812858
lcleveland <lyle.cleveland@proton.me>
2024-11-28 11:56:22 -0600

    change to gnome

--- profiles/dream.nix
+++ profiles/dream.nix
@@ -6,4 +6,5 @@
   system.settings.graphics.enable = true;
   system.settings.boot.kernel.package = pkgs.linuxPackages_zen;
   system.settings.network.hostname = "DREAM";
+  system.settings.desktop_environment.enabled_environment = "gnome";
 }
