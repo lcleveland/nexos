6f3c03ba35d4607a161863249377f1891e7b9ae0
lcleveland <lyle.cleveland@proton.me>
2024-11-28 11:48:34 -0600

    change gtk settings to respect kde

--- accounts/lcleveland/settings/gtk.nix
+++ accounts/lcleveland/settings/gtk.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ... }:
 let
   account = config.accounts.lcleveland.settings;
+  enabled_environment = config.system.settings.desktop_environment.enabled_environment;
 in
 {
-  config = lib.mkIf account.enable {
+  config = lib.mkIf (account.enable && enabled_environment == "kde") {
     home-manager = {
       backupFileExtension = "hm-backup";
       users.lcleveland.gtk = {
