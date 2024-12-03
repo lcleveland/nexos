4f68fa3a59699db8c217522fd9da5f15aef6aa97
lcleveland <lyle.cleveland@proton.me>
2024-12-03 09:20:10 -0600

    virt fix test

--- system/applications/libvirt/virt_manager.nix
+++ system/applications/libvirt/virt_manager.nix
@@ -1,9 +1,18 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   libvirt = config.system.applications.libvirt;
 in
 {
   config = lib.mkIf libvirt.enable {
     programs.virt-manager.enable = true;
+    environment.systemPackages = with pkgs; [
+      (virt-manager.overrideAttrs (old: {
+        nativeBuildInputs = old.nativeBuildInputs ++ [ wrapGAppsHook ];
+        buildInputs = lib.lists.subtractLists [ wrapGAppsHook ] old.buildInputs ++ [
+          gst_all_1.gst-plugins-base
+          gst_all_1.gst-plugins-good
+        ];
+      }))
+    ];
   };
 }
