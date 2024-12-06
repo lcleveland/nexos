c35ee20597705d0d3d9cb907cb4458a07d00b2b1
lcleveland <lyle.cleveland@proton.me>
2024-12-06 13:03:51 -0600

    fix

--- accounts/lcleveland/applications/protonup/enable.nix
+++ accounts/lcleveland/applications/protonup/enable.nix
@@ -1,19 +1,19 @@
 { config, lib, pkgs, ... }:
 let
   account = config.accounts.lcleveland.settings;
-  zoom = config.accounts.lcleveland.applications.zoom;
+  protonup = config.accounts.lcleveland.applications.protonup;
 in
 {
-  options.accounts.lcleveland.applications.zoom = {
+  options.accounts.lcleveland.applications.protonup = {
     enable = lib.mkOption {
       type = lib.types.bool;
       default = true;
-      description = lib.mdDoc "Enable zoom support";
+      description = lib.mdDoc "Enable protonup support";
     };
   };
-  config = lib.mkIf (account.enable && zoom.enable) {
+  config = lib.mkIf (account.enable && protonup.enable) {
     home-manager.users.lcleveland.home.packages = [
-      pkgs.zoom-us
+      pkgs.protonup-qt
     ];
   };
 }
